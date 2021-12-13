76bfedc6e3b69712e09bb24912f6045a79c39eba
Tuhame Astone <astonetuhame@gmail.com>
2021-12-13 19:48:04 +0300

    Update Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
+import Button from "./Button"
+
 const Header = ({title}) => {
+    const onClick = () =>{
+        
+    }
     return (
-        <header>
+        <header className="header">
             <h1>{title}</h1>
+            <Button color="green" text="Add" onClick={onClick}/>
         </header>
     )
 }
