b44a2a8fa5f70264a101fcac60ea67a0b4e26c3a
Tuhame Astone <astonetuhame@gmail.com>
2021-12-13 23:51:52 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import Button from "./Button"
+import { useLocation } from "react-router-dom"
 
 const Header = ({ title, onAdd, showAdd }) => {
-   
+    const location = useLocation()
+
+
     return (
         <header className="header">
             <h1>{title}</h1>
-            <Button onClick={onAdd} color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} />
+
+            {location.pathname === '/' && (<Button onClick={onAdd} 
+                    color={showAdd ? 'red' : 'green'} 
+                    text={showAdd ? 'Close' : 'Add'} />)}
         </header>
     )
 }
