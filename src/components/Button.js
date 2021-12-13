93481571ad491eff836a7c4edbeacadd5f970b86
Tuhame Astone <astonetuhame@gmail.com>
2021-12-13 22:13:57 +0300

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const Button = ({color, text, onClick}) => {
 
     return(
-        <button 
+        <button onClick={onClick}
         style={{ backgroundColor:color }} 
         className="btn">{text}</button>
 
@@ -17,8 +17,7 @@ Button.defaultProps = {
 
 Button.propTypes = {
     text: PropTypes.string,
-    color: PropTypes.string,
-    onClick: PropTypes.func,
+    color: PropTypes.string
 }
 
 export default Button
