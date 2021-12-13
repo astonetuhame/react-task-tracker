e9de99de6632fad35b04881702f0b496708c321b
Tuhame Astone <astonetuhame@gmail.com>
2021-12-13 21:13:53 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,12 +25,24 @@ const App = () => {
         day: "May 14th At 8:30pm",
         reminder: true
       }
-    ]
-  )
+    ])
+
+    //Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+//Toggle Reminder
+const toggleReminder = (id) =>
+{
+  setTasks(tasks.map((task)=> task.id === id ? {...task, reminder: !task.reminder} : task))
+}
+
+
   return (
     <div className="container">
       <Header title="Task Tracker" />
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks To show')}
     </div>
   );
 }
