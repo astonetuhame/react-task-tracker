23645da4770cd8653a57c0de4ba63b6893e223c3
Tuhame Astone <astonetuhame@gmail.com>
2021-12-13 18:49:20 +0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from './components/Header'
+
+const App= () => {
+  return (
+    <div className="container">
+       <Header title="Task Tracker"/>
+    </div>
+  );
+}
+
+export default App;
