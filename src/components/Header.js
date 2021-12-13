e2a0bf1012833f72470d2dc0a80c27042db5071d
Tuhame Astone <astonetuhame@gmail.com>
2021-12-13 22:14:02 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,11 @@
 import Button from "./Button"
 
-const Header = ({title}) => {
-    const onClick = () =>{
-        
-    }
+const Header = ({ title, onAdd, showAdd }) => {
+   
     return (
         <header className="header">
             <h1>{title}</h1>
-            <Button color="green" text="Add" onClick={onClick}/>
+            <Button onClick={onAdd} color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} />
         </header>
     )
 }
